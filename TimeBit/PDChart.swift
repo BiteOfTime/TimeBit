54f1b94ae33db1bb3597612f8e360a7063bc74c3
Namrata Mehta <namrata@Gauravs-MBP.hsd1.ca.comcast.net>
2017-05-06 23:15:23 -0700

    adding the classes for graph

---
+++ TimeBit/PDChart.swift
@@ -0,0 +1,35 @@
+//
+//  PDChart.swift
+//  TimeBit
+//
+//  Created by Namrata Mehta on 5/6/17.
+//  Copyright © 2017 BiteOfTime. All rights reserved.
+//
+
+import UIKit
+
+let yMargin: Double = 10
+let xMargin: Double = 10
+
+class PDChart: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func strokeChart() {
+        
+    }
+    
+    override func draw(_ rect: CGRect)
+    {
+        // Drawing code
+        
+    }
+
+}
